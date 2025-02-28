3789e5dc8db5544673a554435c145be58da1d122
AlfredMoraMora <1248649@cegepoutaouais.qc.ca>
2025-02-28 14:15:06 -0500

    Ajout du code dans fichier. 1

--- Silksong_waiter.py
+++ Silksong_waiter.py
@@ -1,2 +1,28 @@
+class Silksong_waiter:
+    def __init__(self, nom: str, sanity: int):
+        self.nom = nom
+        self.sanity = sanity
+
+    @property
+    def nom(self):
+        return self.nom
+
+    @nom.setter
+    def nom(self, nouveau_nom):
+        self.nom = nouveau_nom
+
+    @property
+    def sanity(self):
+        return self.sanity
+
+    @sanity.setter
+    def sanity(self, new_sanity):
+        self.sanity = new_sanity
+
+    def pass_day(self, sanity):
+        pass
+
+    def kms(self, sanity):
+        pass
 
 
