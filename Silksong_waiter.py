4221e4fb04d5c51197a7b2a5bfb8bcf864a46c5a
AlfredMoraMora <1248649@cegepoutaouais.qc.ca>
2025-03-05 11:54:10 -0500

    Some improvements to Silksong_waiter.py

--- Silksong_waiter.py
+++ Silksong_waiter.py
@@ -1,3 +1,5 @@
+import sys
+
 class Silksong_waiter:
     def __init__(self, nom: str, sanity: int):
         self.nom = nom
@@ -17,20 +19,22 @@ class Silksong_waiter:
 
     @sanity.setter
     def sanity(self, new_sanity):
-        if isinstance(self.sanity, int):
+        if isinstance(new_sanity, int):
             self._sanity = new_sanity
         else:
-            raise ValueError("NaN")
+            raise ValueError("La valeur doit être un chiffre entier. ")
         
 
-    def pass_day(self, sanity):
-        if self.sanity <= 10:
-            kms(sanity)
+    def pass_day(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Sanity doit être un nombre entier. ")
+        if value <= 10:
+            self.kms(value)
         else:
             self.sanity += 5
 
-    def kms(self, sanity):
-        self.sanity = sanity
+    def kms(self, value):
+        self.sanity = value
         print("Tu t'est suicidé, rip")
         sys.exit(0)
 
